548f12d82d52b76739d185aca864ecf5d70e7bb5
nikita-panacea <nikita.trainee@panaceainfosec.com>
2025-03-12 15:05:09 +0530

    update phi4 image test

--- test_code/phi4-img-test.py
+++ test_code/phi4-img-test.py
@@ -12,9 +12,9 @@ from azure.core.credentials import AzureKeyCredential
 
 token = os.environ["GITHUB_TOKEN"]
 endpoint = "https://models.inference.ai.azure.com"
-# model_name = "Phi-4-multimodal-instruct"
+model_name = "Phi-4-multimodal-instruct"
 # model_name = "Phi-3.5-vision-instruct"
-model_name = "gpt-4o"
+# model_name = "gpt-4o"
 
 client = ChatCompletionsClient(
     endpoint=endpoint,
@@ -25,10 +25,19 @@ response = client.complete(
     messages=[
         UserMessage(
             content=[
-                TextContentItem(text="You are an Network security expert. Write a analysis report explaining the data flow."),
+                TextContentItem(text="""
+        You are an expert in PCI-DSS compliance network security. You are provided with image evidence. 
+        The image evidence has been provided by the audited organization as part of PCI DSS audit to prove 
+        that they fulfil a particular control of PCI DSS framework.
+        Provide a detailed report on the given image for compliance analysis:
+        - List and name all components present in the image.
+        - Describe the information presented in the image in detail.
+        - Explain the information on security measures from the image.
+        - Identify any potential vulnerabilities or security risks.
+        """),
                 ImageContentItem(
                     image_url=ImageUrl.load(
-                        image_file="card_decryption_flow.jpg",
+                        image_file="Connfido Network Diagram.png",
                         image_format="jpg",
                         detail=ImageDetailLevel.HIGH)
                 ),
